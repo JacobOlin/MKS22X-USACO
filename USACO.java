ba4a363722afe875ba9c18f7c99d58eabf0c1e92
Jacob Olin <jolin00@stuy.edu>
2019-03-06 11:42:22 -0500

    Started writing the function to make all of the moves

--- USACO.java
+++ USACO.java
@@ -71,6 +71,11 @@ private static int[][]moves;
   }
 
   public static void makeMove(int[] a) {
-
+    int max = 0;
+    for (int i = 0;i < 3;i += 1) {
+      for (int j = 0;j < 3;j += 1) {
+        
+      }
+    }
   }
 }
